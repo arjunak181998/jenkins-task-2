5f147d32adc10e0a93ebe976636f5ecd02b10e77
Arjun Kumar <alwinarjunak@gmail.com>
2026-06-29 13:48:56 +0000

    Initial Jenkins task

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "--------------------------------"
+echo "Jenkins Build Successful"
+echo "Current Date:"
+date
+echo "Current User:"
+whoami
+echo "Current Directory:"
+pwd
+echo "Hostname:"
+hostname
+echo "--------------------------------"
